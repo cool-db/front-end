6872933a15611cbc6f496c9569ad5adea4356ece
monkingxue <1360662499@qq.com>
2017-06-01 21:53:17 +0800

    [add] 引入自定义主题

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
+import '../theme/index.css'
 
 Vue.use(ElementUI)
 Vue.config.productionTip = false
