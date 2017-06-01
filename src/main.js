bc9a952af8b4e747ccc2f94eeafd77f1cbc29a90
monkingxue <1360662499@qq.com>
2017-06-01 19:18:32 +0800

    [add]增加对 elm-ui 的支持

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
 
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
