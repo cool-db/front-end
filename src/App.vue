f750688bf7fe77aa8e4177b9eacee60396eb3ae9
monkingxue <1360662499@qq.com>
2017-07-10 12:21:59 +0800

    [merge] merge dustark first branch

--- src/App.vue
+++ src/App.vue
@@ -11,15 +11,6 @@
 </script>
 
 <style scoped lang="scss">
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
-
     .router-fade-enter-active, .router-fade-leave-active {
         transition: opacity .3s;
     }
