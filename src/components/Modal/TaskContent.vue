d002535ae1db46f7c74e1f372b711f84c8ff9bde
monkingxue <1360662499@qq.com>
2017-07-11 00:14:17 +0800

    [add] add text editor component

--- src/components/Modal/TaskContent.vue
+++ src/components/Modal/TaskContent.vue
@@ -1,10 +1,15 @@
 <template>
-    <text-edit></text-edit>
+    <text-edit :title.sync="taskTitle"></text-edit>
 </template>
 
 <script>
   import TextEdit from '../TextEdit.vue'
   export default {
+    data () {
+      return {
+        taskTitle: ''
+      }
+    },
     components: {
       TextEdit
     }
