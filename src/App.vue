f77be11131323899fe60b8972c7a9147a638ad60
monkingxue <1360662499@qq.com>
2017-07-10 22:54:44 +0800

    [add] add task content modal

--- src/App.vue
+++ src/App.vue
@@ -10,11 +10,17 @@
   export default {}
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
     .router-fade-enter-active, .router-fade-leave-active {
         transition: opacity .3s;
     }
     .router-fade-enter, .router-fade-leave-active {
         opacity: 0;
+    }
+
+    .el-dialog__wrapper {
+        .el-dialog--small {
+            background: #F8F8F8;
+        }
     }
 </style>
