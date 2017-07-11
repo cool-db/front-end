93a76298dfb23c17541998387e959146e62ab417
Angel <anqizhu97@gmail.com>
2017-07-11 14:18:03 +0800

    左下角选择器

---
+++ src/components/Selector.vue
@@ -0,0 +1,43 @@
+<template>
+  <el-card id="selector">
+    <div id="personalInformation" onclick="personal">
+      个人信息
+    </div>
+    <div id="accountPassward" onclick="account">
+      账号密码
+    </div>
+  </el-card>
+</template>
+
+<style>
+  #selector{
+    display: flex;
+    flex-direction: column;
+    width: 300px;
+    height: 350px;
+  }
+  
+  #personalInformation{
+    display: flex;
+    align-items: center;
+    height: 65px;
+    width: 300px;
+    margin-left: -20px;
+    margin-top: -20px;
+    background-color: #E7F1F0;
+    color: #3E5568;
+    padding-left: 20px;
+  }
+  
+  #accountPassward{
+    display: flex;
+    align-items: center;
+    height: 65px;
+    width: 300px;
+    margin-left: -20px;
+    background-color: #FFFFFF;
+    color: #3E5568;
+    padding-left: 20px;
+  }
+  
+</style>
