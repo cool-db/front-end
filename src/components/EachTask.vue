2e7e16bd896c3a853b8599afeb2086788bcb7106
Simon Gavins Lee <simongavinslee0013@icloud.com>
2017-07-12 14:33:21 +0800

    [update] my我的 执行创建参与 样式修改

--- src/components/EachTask.vue
+++ src/components/EachTask.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="each-task-main">
 
-    <el-checkbox v-model="checked">
-        {{taskname}} {{projname}}
+      <el-checkbox v-model="checked">
+          <span class="font-style-1">{{taskname}}</span>
+          <span class="font-style-2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{projname}}</span>
         <div class="sub-task">属于任务：{{dadtask}}</div>
-    </el-checkbox>
+      </el-checkbox>
 
-    <span><span style="color: #3cc">{{duedate}}</span> 截止</span>
 
+      <span class="font-style-2"><span style="color: #3cc">{{duedate}}</span> 截止</span>
 
   </div>
 </template>
@@ -31,13 +32,26 @@
     display: flex;
     display: -webkit-flex;
     justify-content: space-between;
-    padding: 10px 20px;
+    padding: 0px 20px;
+    margin: 10px 0;
     border-left: 10px red solid;
     border-right: 10px #fff solid;
     color: #888;
   }
 
   .sub-task{
-    margin-left: 26px;
+    margin-left: 23px;
+    font-size: 14px;
+    color: #888;
+  }
+
+  .font-style-1{
+    font-size: 14px;
+    color: #565656;
+  }
+
+  .font-style-2{
+    color: #888;
+    font-size: 12px;
   }
 </style>
