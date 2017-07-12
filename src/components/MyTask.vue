7260b92b0727850d2981531f97a8d7ee66088b6b
Simon Gavins Lee <simongavinslee0013@icloud.com>
2017-07-12 13:31:07 +0800

    my我的 执行创建参与

---
+++ src/components/MyTask.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="main">
+    <div class="main-box">
+
+      <div class="head-bar">
+        <el-tabs v-model="activeName" @tab-click="handleClick">
+          <el-tab-pane label="我执行的" name="first">
+            <each-task></each-task>
+          </el-tab-pane>
+          <el-tab-pane label="我创建的" name="second">
+            <each-task></each-task><each-task></each-task><each-task></each-task>
+          </el-tab-pane>
+          <el-tab-pane label="我参与的" name="third">
+            <each-task></each-task><each-task></each-task>
+          </el-tab-pane>
+        </el-tabs>
+      </div>
+
+      <div class="task-area">
+
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+  import EachTask from '@/components/EachTask.vue'
+  export default {
+    components: {EachTask},
+    data () {
+      return {
+        taskcount: 5
+      }
+    },
+    methods: {
+      handleClick (tab, event) {
+        console.log(tab, event)
+      }
+    }
+  }
+</script>
+
+<style>
+  .main{
+    display: flex;
+    display: -webkit-flex;
+    justify-content: center;
+  }
+
+  .main-box{
+    width: 800px;
+  }
+
+  .head-bar{
+    margin: 20px 30px 0 30px;
+    padding-bottom: 10px;
+    color: #888;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import RecentTask from '@/components/RecentTask.vue'
+import MyTask from '@/components/MyTask.vue'
 
 Vue.use(Router)
 
 const routes = [
   {
     path: '/',
-    component: RecentTask
+    component: MyTask
   }
 ]
 
