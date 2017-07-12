3988e1c9054868af7b43e29a09441798267feae2
monkingxue <1360662499@qq.com>
2017-07-12 18:56:49 +0800

    [fix] fix schedule reference problem

--- src/pages/Schedule.vue
+++ src/pages/Schedule.vue
@@ -1,37 +1,22 @@
 <template>
-  <div class="schedule">
-    <div class="bar">
-      <div class="addSchedule">
-        <img class="picture" :src="addParticipator">
-        <div class="text">添加日程</div>
-      </div>
+    <div class="schedule">
+        <div class="bar">
+            <div class="addSchedule">
+                <img class="picture" :src="addParticipator">
+                <div class="text">添加日程</div>
+            </div>
+        </div>
+
+        <date-schedule v-for="(item, index) in fake"
+                       :key="index"
+                       :date="item.date">
+        </date-schedule>
     </div>
-
-    <date-schedule v-for="(item, index) in fake"
-               :key="index"
-               :date="item.date">
-    </date-schedule>
-
-    <div class="pages">
-      <el-pagination
-        small
-        layout="prev, pager, next"
-        :total="50">
-      </el-pagination>
-
-      <div class="line">
-      </div>
-    </div>
-
-
-  </div>
-
-
 </template>
 
 <script>
   import addParticipator from '@/assets/icons/new_item/add-participator.png'
-  import DateSchedule from './DateSchedule.vue'
+  import DateSchedule from 'COMPONENTS/DateSchedule.vue'
 
   export default{
     data () {
@@ -53,47 +38,54 @@
 </script>
 
 <style>
-  .schedule {
-    margin-top:14px;
-    display:flex;
-    /*height:500px;*/
-    flex-direction:column;
-    align-items: center;
-    justify-content: space-between;
+    .schedule {
+        margin-top: 14px;
+        display: flex;
+        /*height:500px;*/
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-between;
 
-  }
-  .bar {
-    display:flex;
-    flex-direction:row;
-    align-items: center;
-    justify-content: center;
-    width:1040px;
-    height:40px;
-    background-color:#E7F1F0;
-    border-radius: 4px;
-  }
-  .addSchedule{
-    display:flex;
-    flex-direction:row;
-    align-items: center;
-    justify-content: center;
-  }
-  .picture{
-    width:19px;
-    height:19px;
-  }
-  .text{
-    margin-left:10px;
-    color:#33CCCC;
-  }
-  .pages{
-    display:flex;
-    flex-direction:column;
-    margin-top:6px;
-  }
-  .line{
-    height:11px;
-    width:187px;
-    background-color:#33CCCC;
-  }
+    }
+
+    .bar {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        width: 1040px;
+        height: 40px;
+        background-color: #E7F1F0;
+        border-radius: 4px;
+    }
+
+    .addSchedule {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        cursor: pointer;
+    }
+
+    .picture {
+        width: 19px;
+        height: 19px;
+    }
+
+    .text {
+        margin-left: 10px;
+        color: #33CCCC;
+    }
+
+    .pages {
+        display: flex;
+        flex-direction: column;
+        margin-top: 6px;
+    }
+
+    .line {
+        height: 11px;
+        width: 187px;
+        background-color: #33CCCC;
+    }
 </style>
