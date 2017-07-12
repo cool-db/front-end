497e2f86984e8cf58cc63f2091fc28a0c207e18d
崔鹤洁 <2410394986@qq.com>
2017-07-12 14:54:31 +0800

    日程组件

---
+++ src/components/Schedule.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="schedule">
+    <div class="bar">
+      <div class="addSchedule">
+        <img class="picture" :src="addParticipator">
+        <div class="text">添加日程</div>
+      </div>
+    </div>
+
+    <date-schedule v-for="(item, index) in fake"
+               :key="index"
+               :date="item.date">
+    </date-schedule>
+
+    <div class="pages">
+      <el-pagination
+        small
+        layout="prev, pager, next"
+        :total="50">
+      </el-pagination>
+
+      <div class="line">
+      </div>
+    </div>
+
+
+  </div>
+
+
+</template>
+
+<script>
+  import addParticipator from '@/assets/icons/new_item/add-participator.png'
+  import DateSchedule from './DateSchedule.vue'
+
+  export default{
+    data () {
+      return {
+        addParticipator,
+        fake: [
+          {
+            date: '5月9日'
+          },
+          {
+            date: '5月10日'
+          }]
+      }
+    },
+    components: {
+      DateSchedule
+    }
+  }
+</script>
+
+<style>
+  .schedule {
+    margin-top:50px;
+    display:flex;
+    /*height:500px;*/
+    flex-direction:column;
+    align-items: center;
+    justify-content: space-between;
+
+  }
+  .bar {
+    display:flex;
+    flex-direction:row;
+    align-items: center;
+    justify-content: center;
+    width:1040px;
+    height:40px;
+    background-color:#E7F1F0;
+    border-radius: 4px;
+  }
+  .addSchedule{
+    display:flex;
+    flex-direction:row;
+    align-items: center;
+    justify-content: center;
+  }
+  .picture{
+    width:19px;
+    height:19px;
+  }
+  .text{
+    margin-left:10px;
+    color:#33CCCC;
+  }
+  .pages{
+    display:flex;
+    flex-direction:column;
+    margin-top:30px;
+  }
+  .line{
+    height:8px;
+    width:187px;
+    background-color:#33CCCC;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -8,22 +8,24 @@ import Router from 'vue-router'
 // import personalInfomation from 'COMPONENTS/PersonalInfomation'
 // import Selector from 'COMPONENTS/Selector'
 // import PersonalSetting from 'COMPONENTS/PersonalSetting'
-import login from 'COMPONENTS/Login'
-import register from 'COMPONENTS/Register'
+// import login from 'COMPONENTS/Login'
+// import register from 'COMPONENTS/Register'
+// import dateSchedule from 'COMPONENTS/DateSchedule'
+import Schedule from 'COMPONENTS/Schedule'
 
 Vue.use(Router)
 
 const routes = [
   {
     path: '/a',
-    component: login,
+    component: Schedule,
     name: 'a'
-  },
-  {
-    path: '/d',
-    component: register,
-    name: 'd'
   }
+  // {
+  //   path: '/d',
+  //   component: register,
+  //   name: 'd'
+  // }
   // {
   //   path: '/d',
   //   component: PersonalSetting,
