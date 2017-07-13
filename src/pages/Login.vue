9f8cd527b6d4a8c9591659818d64504082c17cb9
Simon Gavins Lee <simongavinslee0013@icloud.com>
2017-07-14 01:37:05 +0800

    启用全新logo 更新了登陆和注册页面样式（为避免数据上错误，旧代码注释掉）

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,35 +1,36 @@
 <template>
-    <div class="login">
-        <div class="content">
-            <img class="picture" :src="logo">
-            <el-form class="myform" :model="ruleForm" ref="ruleForm" label-width="100px">
-                <el-form-item label="邮箱" prop="pass">
-                    <el-input v-model="ruleForm.email" auto-complete="off"></el-input>
-                </el-form-item>
-                <el-form-item label="密码" prop="checkPass">
-                    <el-input type="password" v-model="ruleForm.password" auto-complete="off"></el-input>
-                </el-form-item>
-                <el-form-item>
-                    <el-button class="button" type="primary" @click="submitForm('ruleForm')">提交</el-button>
-                </el-form-item>
-            </el-form>
-            <div class="line">
-            </div>
+  <div class="login">
+    <div class="content">
+      <div class="login-logo"><img class="picture" :src="logo"></div>
+      <el-form class="myform" :model="ruleForm" ref="ruleForm" label-width="100px">
+        <el-form-item prop="pass">
+          <el-input v-model="ruleForm.email" auto-complete="off" placeholder="邮箱"></el-input>
+        </el-form-item>
+        <el-form-item prop="checkPass">
+          <el-input type="password" v-model="ruleForm.password" auto-complete="off" placeholder="密码"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="button" type="primary" @click="submitForm('ruleForm')">提交</el-button>
+        </el-form-item>
+      </el-form>
 
-            <div class="textButton">
-                <div class="account"> 还没有账户？</div>
-                <el-button class="text" type="text">
-                    <router-link to="/register" replace style="text-decoration: none; color:#33CCCC">注册新账号</router-link>
-                </el-button>
-            </div>
-        </div>
+      <!--<div class="line"></div>-->
+      <div class="textButton">
+        <div class="account">还没有账户?</div>
+
+          <router-link to="/register" replace style="text-decoration: none; color:#33CCCC">
+            <el-button class="text" type="text">注册新账号</el-button>
+          </router-link>
+
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 
   import project from '@/assets/icons/nav_bar/project.png'
-  import logo from '@/assets/logo.png'
+  import logo from '@/assets/icons/index/Groupup.png'
 
   import { login } from 'API/userApi'
 
@@ -72,77 +73,56 @@
 
 <style scoped>
 
-    .myform {
-        margin-left: -90px;
-    }
+  .myform {
+    margin-left: -90px;
+  }
 
-    .login {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-    }
+  .login {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
 
-    .content {
-        align-items: center;
-    }
+  .login-logo{
+    display: flex;
+    justify-content: center;
+  }
 
-    .picture {
-        height: 78px;
-        width: 280px;
-        margin-top: -5px;
-        margin-left: 0px;
-        margin-bottom: 10px;
-    }
+  .picture {
+    height: 128px;
+    width: 128px;
+    margin: 0 20px;
+  }
 
-    .mail {
-        height: 60px;
-        width: 280px;
-        display: flex;
-        flex-direction: row;
-        margin-top: 20px;
-    }
+  .button {
+    width: 280px;
+    height: 40px;
+    margin-top: 20px;
+  }
 
-    .password {
-        height: 60px;
-        width: 280px;
-        display: flex;
-        flex-direction: row;
-    }
+  /*.line {*/
+    /*width: 280px;*/
+    /*height: 1px;*/
+    /*margin-top: 50px;*/
+    /*margin-left: 10px;*/
+    /*background-color: #C0C0C0;*/
+  /*}*/
 
-    .button {
-        width: 280px;
-        height: 40px;
-        margin-top: 20px;
-    }
+  .textButton {
+    display: flex;
+    justify-content: center;
+    margin-top: 10px;
+  }
 
-    .line {
-        width: 280px;
-        height: 1px;
-        margin-top: 50px;
-        margin-left: 10px;
-        background-color: #C0C0C0;
-    }
+  .account {
+    font-size: 14px;
+    margin-top: 10px;
+  }
 
-    .textButton {
-        display: flex;
-        flex-direction: row;
-        margin-top: 10px;
-    }
-
-    .account {
-        font-size: 14px;
-        display: flex;
-        color: black;
-        margin-top: 10px;
-        margin-left: 50px;
-    }
-
-    .text {
-        color: #33CCCC;
-        margin-top: 1px;
-        margin-left: 20px;
-    }
-
-
+  .text {
+    color: #33CCCC;
+    margin-top: 1px;
+    margin-left: 20px;
+  }
 </style>
