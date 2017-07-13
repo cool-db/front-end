177ccb1f518121f71c176a615e98b9cd2bd88b7b
Simon Gavins Lee <simongavinslee0013@icloud.com>
2017-07-13 13:01:41 +0800

    [add] cover-demo.jpg

---
+++ src/components/HomeCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="home-card-box">
+    <el-card class="single-proj">
+      <div class="proj-name">{{projName}}</div>
+      <div class="proj-note">{{projNote}}</div>
+    </el-card>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+      }
+    },
+    props: ['projName', 'projNote']
+  }
+</script>
+
+<style scoped>
+  .home-card-box{
+    margin: 5px 8px;
+    cursor: pointer;
+    background-image: url(/src/assets/cover-demo.jpg);
+  }
+
+  .single-proj{
+    width: 254px;
+    height: 148px;
+  }
+
+  .proj-name{
+    font-size: 18px;
+    padding-bottom: 5px;
+    color: #888;
+  }
+
+  .proj-note{
+    font-size: 16px;
+    padding-top: 5px;
+    color: #888;
+  }
+</style>
